e450c814b24506c38a069f7c3d564d60e45a34e6
isa_belly07 <isabellychistinny1213@gmail.com>
2024-09-10 20:00:30 -0300

    Descrição das alterações feitas

---
+++ Isabelly/Java_vscode/aulas/Sincronizar/src/app_banheiro/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        Banheiro banheiro = new Banheiro();
+
+        Thread convidado1 = new Thread(new Numero1 (banheiro),  "João");
+        Thread convidado2 = new Thread(new Numero2 (banheiro),  "andre");
+        Thread convidado3 = new Thread(new Numero1 (banheiro),  "natusoe");
+        Thread convidado4 = new Thread(new Numero2 (banheiro),  "andrade");
+        Thread convidado5 = new Thread(new Numero1 (banheiro),  "maudivas");
+    
+        convidado1.start();
+        convidado2.start();
+        convidado3.start();
+        convidado4.start();
+        convidado5.start();
+    }
+
+
+}
