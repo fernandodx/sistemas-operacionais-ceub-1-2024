213d73cdcf2e8fb6c8fa51030b19bd72ba7ce39f
Bruno Tavares Cordeiro <bruno.cordeiro@dinfor.uniceub>
2024-09-17 21:04:40 -0300

    atualização

--- Bruno Tavares Cordeiro/java_vscode/aulas/sincronizar/sincronizar/src/App.java
+++ Bruno Tavares Cordeiro/java_vscode/aulas/sincronizar/sincronizar/src/App.java
@@ -7,11 +7,14 @@ public class App {
         Thread convidado3 = new Thread(new Numero1(banheiro), "Marcus");
 
         Thread convidado4 = new Thread(new Numero2(banheiro), "Jeferson");
+        Thread limpeza = new Thread(new Limpeza(banheiro), "Lucas da limpeza");
 
         convidado1.start();
         convidado2.start();
         convidado3.start();
         convidado4.start();
+
+        limpeza.start();
 
     }
 }
---
+++ Bruno Tavares Cordeiro/java_vscode/aulas/sincronizar/sincronizar/src/Limpeza.java
@@ -0,0 +1,22 @@
+
+public class Limpeza implements Runnable {
+
+    private Banheiro banheiro;
+
+    public Limpeza(Banheiro banheiro) {
+        this.banheiro = banheiro;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+        }
+
+        this.banheiro.limpar();
+    }
+
+}
