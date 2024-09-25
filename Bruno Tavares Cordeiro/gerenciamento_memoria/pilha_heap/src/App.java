d4ce3d0f7bca67f4e88f303d7e70a809276d7c34
Bruno Tavares Cordeiro <bruno.cordeiro@dinfor.uniceub>
2024-09-24 21:51:38 -0300

    Atualização

---
+++ Bruno Tavares Cordeiro/gerenciamento_memoria/pilha_heap/src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Departamento departamento = new Departamento("Ciência");
+        Disciplina disciplina = new Disciplina("Sitemas Operacionais", departamento);
+
+        Disciplina disciplina2 = new Disciplina("Sistemas Operacionais", departamento);
+
+        System.out.println(disciplina == disciplina2);
+        System.out.println("Disciplina" + disciplina);
+        System.out.println("Disciplina 2" + disciplina2);
+
+        disciplina = disciplina2;
+
+        System.out.println(disciplina == disciplina2);
+        System.out.println("Disciplina" + disciplina);
+        System.out.println("Disciplina 2" + disciplina2);
+
+        // Adiciona a diciplina ao departamento
+
+        // departamento.AdicionarDiciplina(disciplina);
+
+        // tentar imprimir os detalhes do depatarmento
+        // departamento.imprimirDetalhes();
+    }
+}
