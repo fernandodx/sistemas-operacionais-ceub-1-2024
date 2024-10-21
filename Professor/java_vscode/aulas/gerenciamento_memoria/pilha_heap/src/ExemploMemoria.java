fb1ea2bbb2fca8d8967ff5235fec2f61052b2d68
Fernando Dias <fernando.dias@sicoob.com.br>
2024-10-21 09:43:42 -0300

    Testes- Aula de controle de memoria

--- Professor/java_vscode/aulas/gerenciamento_memoria/pilha_heap/src/ExemploMemoria.java
+++ Professor/java_vscode/aulas/gerenciamento_memoria/pilha_heap/src/ExemploMemoria.java
@@ -1,12 +1,21 @@
+import java.net.http.HttpClient;
+import java.util.Scanner;
+
 public class ExemploMemoria {
 
     public static void main(String[] args) {
         StringBuilder resultado = new StringBuilder();
 
-        for (int i = 0; i <= 10000; i++) {
+        for (int i = 0; i <= 1000; i++) {
             resultado.append(" ").append(i);
+
         }
         System.out.println(resultado.toString());
+
+        // System.out.println(Departamento.class.getClassLoader());
+        // System.out.println(Disciplina.class.getClassLoader());
+        // System.out.println(HttpClient.class.getClassLoader());
+
     }
 
 }
