40a7f07a281332ea8d17845af75515cdbe6182a9
Fernando Dias <fernando.dias@sicoob.com.br>
2024-11-08 16:51:35 -0300

    inicio projeto flutter

--- Professor/java_vscode/aulas/gerenciamento_memoria/pilha_heap/src/ExemploMemoria.java
+++ Professor/java_vscode/aulas/gerenciamento_memoria/pilha_heap/src/ExemploMemoria.java
@@ -1,12 +1,19 @@
+import java.net.http.*;
+
 public class ExemploMemoria {
 
     public static void main(String[] args) {
-        StringBuilder resultado = new StringBuilder();
+        // StringBuilder resultado = new StringBuilder();
 
-        for (int i = 0; i <= 10000; i++) {
-            resultado.append(" ").append(i);
-        }
-        System.out.println(resultado.toString());
+        // for (int i = 0; i <= 10000; i++) {
+        // resultado.append(" ").append(i);
+        // }
+        // System.out.println(resultado.toString());
+
+        System.out.println(Departamento.class.getClassLoader());
+        System.out.println(Disciplina.class.getClassLoader());
+        System.out.println(HttpClient.class.getClassLoader());
+
     }
 
 }
